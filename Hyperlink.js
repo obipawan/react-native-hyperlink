68dc24d436fa51ad41473852d3787ee1dee39ac4
gnl <gl@lipov.net>
2016-05-09 00:57:08 +0200

    Return the unprocessed component when the linkify code throws an exception

--- Hyperlink.js
+++ Hyperlink.js
@@ -40,21 +40,28 @@ const Hyperlink = React.createClass({
     }
     let elements = [];
     let _lastIndex = 0;
-    linkify.match(component.props.children).forEach(({index, lastIndex, text, url}) =>{
-      let nonLinkedText = component.props.children.substring(_lastIndex, index);
-      if (nonLinkedText){
-        elements.push(nonLinkedText);
-      }
-      _lastIndex = lastIndex;
-      elements.push(
-        <Text {...component.props}
-              style={[component.props.style], [this.props.linkStyle]}
-              onPress={() => {(this.props.onPress) ? this.props.onPress(url) : {}}}
-              key={url}>{this.props.linkText || text}</Text>
-      );
-    });
-    elements.push(component.props.children.substring(_lastIndex, component.props.children.length));
-    return React.cloneElement(component, component.props, elements);
+
+    try {
+      linkify.match(component.props.children).forEach(({index, lastIndex, text, url}) => {
+        let nonLinkedText = component.props.children.substring(_lastIndex, index);
+        if (nonLinkedText) {
+          elements.push(nonLinkedText);
+        }
+        _lastIndex = lastIndex;
+        elements.push(
+          <Text {...component.props}
+            style={[component.props.style], [this.props.linkStyle]}
+              onPress={() => {
+            (this.props.onPress) ? this.props.onPress(url) : {}
+          }}
+            key={url}>{this.props.linkText || text}</Text>
+        );
+      });
+      elements.push(component.props.children.substring(_lastIndex, component.props.children.length));
+      return React.cloneElement(component, component.props, elements);
+    } catch (err) {
+      return component;
+    }
   },
 
   parse(component){
