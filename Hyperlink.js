1288fc25183ff613172811d6609dae812d8f7108
Justin Hamade <justinffs@users.noreply.github.com>
2017-08-31 07:32:54 -0700

    Change PropType imports (#16)

--- Hyperlink.js
+++ Hyperlink.js
@@ -2,7 +2,8 @@
 * @providesModule Hyperlink
 **/
 
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {
 	View,
 	Text,
