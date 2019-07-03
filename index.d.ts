cc8289371a76275536b3f479a4ac2a28e55973ed
Thibault Malbranche <thibault.malbranche@epitech.eu>
2019-07-03 17:38:01 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,12 @@ import { Component } from 'react';
 import { StyleProp, TextStyle } from 'react-native';
 
 interface Props {
-  linkDefault: boolean;
-  linkify: object;
+  linkDefault?: boolean;
+  linkify?: object;
   linkStyle?: StyleProp<TextStyle>;
-  linkText: string | ((text: string) => string);
-  onPress: (url: string, text: string) => void;
-  onLongPress: (url: string, text: string) => void;
+  linkText?: string | ((text: string) => string);
+  onPress?: (url: string, text: string) => void;
+  onLongPress?: (url: string, text: string) => void;
 }
 
 declare class HyperLink extends Component<Props> {}
