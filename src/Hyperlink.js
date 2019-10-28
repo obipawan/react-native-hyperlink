35ef37998eee2b9d827514a0739e3ef553b49cb6
kuzkokov <kuzkokov@gmail.com>
2019-10-28 20:46:50 +0300

    Implement opportunity to set the wrapper component

--- src/Hyperlink.js
+++ src/Hyperlink.js
@@ -4,10 +4,10 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { 
-	View, 
-	Text, 
-	Linking, 
+import {
+	View,
+	Text,
+	Linking,
 	Platform
 } from 'react-native'
 import mdurl from 'mdurl';
@@ -28,26 +28,29 @@ class Hyperlink extends Component {
   }
 
   render() {
-    const { ...viewProps } = this.props
-    delete viewProps.onPress
-    delete viewProps.linkDefault
-    delete viewProps.onLongPress
-    delete viewProps.linkStyle
-		
+    const { ...wrapperProps } = this.props
+    delete wrapperProps.onPress
+    delete wrapperProps.linkDefault
+    delete wrapperProps.onLongPress
+    delete wrapperProps.linkStyle
+    delete wrapperProps.wrapperComponent
+
+    const Wrapper = this.props.wrapperComponent || View;
+
     return (
-      <View { ...viewProps } style={ this.props.style }>
+      <Wrapper { ...wrapperProps } style={ this.props.style }>
         { !this.props.onPress && !this.props.onLongPress && !this.props.linkStyle
           ? this.props.children
           : this.parse(this).props.children }
-      </View>
+      </Wrapper>
     )
   }
 
   isTextNested(component) {
-    if (!React.isValidElement(component)) 
+    if (!React.isValidElement(component))
       throw new Error('Invalid component')
     let { type: { displayName } = {} } = component
-    if (displayName !== 'Text') 
+    if (displayName !== 'Text')
       throw new Error('Not a Text component')
     return typeof component.props.children !== 'string'
   }
@@ -80,7 +83,7 @@ class Hyperlink extends Component {
 
         const clickHandlerProps = {}
         if (OS !== 'web') {
-          clickHandlerProps.onLongPress = this.props.onLongPress 
+          clickHandlerProps.onLongPress = this.props.onLongPress
             ? () => this.props.onLongPress(url, text)
             : undefined
         }
@@ -141,6 +144,7 @@ Hyperlink.propTypes = {
   ]),
   onPress: PropTypes.func,
   onLongPress: PropTypes.func,
+  wrapperComponent: PropTypes.elementType,
 }
 
 export default class extends Component {
@@ -160,7 +164,7 @@ export default class extends Component {
 
   render () {
     const onPress = this.handleLink || this.props.onPress
-	if (this.props.linkDefault) 
+	if (this.props.linkDefault)
 		return <Hyperlink { ...this.props } onPress={ onPress }/>
     return <Hyperlink { ...this.props } />
   }
